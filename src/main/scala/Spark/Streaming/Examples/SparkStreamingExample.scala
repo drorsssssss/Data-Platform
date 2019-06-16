ad22833718b3e8801570ccdad6bc5399253840d0
dror <sivan@adobe.com>
2019-06-16 12:16:11 +0300

    Join between stream and static dim

--- src/main/scala/Spark/Streaming/Examples/SparkStreamingExample.scala
+++ src/main/scala/Spark/Streaming/Examples/SparkStreamingExample.scala
@@ -1,13 +1,13 @@
 package Spark.Streaming.Examples
 
 import org.apache.spark._
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.dstream.DStream
 
 class SparkStreamingExample {
 
-  def process(conf:SparkConf, ssc:StreamingContext) = {
+  def process(conf:SparkConf, ssc:StreamingContext,rddOut:RDD[(String,String)]) = {
 
     val lines = ssc.socketTextStream("localhost", 4444)
     val words = lines.flatMap(_.split(" "))
@@ -19,7 +19,10 @@ class SparkStreamingExample {
       val df = rdd.toDF("word")
       df.createOrReplaceTempView("words")
 
-      val dfCount = spark.sql("select word,count(1) from words group by word")
+      val dim = rddOut.toDF("id","name")
+      dim.createOrReplaceTempView("dim")
+
+      val dfCount = spark.sql("select word,name from words as w left join dim as d on w.word=d.id")
       dfCount.show()
 
 
@@ -37,8 +40,10 @@ object SparkStreamingExample {
     val conf = new SparkConf().setMaster("local[*]").setAppName("NetworkWordCount")
     val ssc = new StreamingContext(conf, Seconds(1))
 
+    val statDF = ssc.sparkContext.parallelize(Seq(("1","dror"),("1","nitzan"),("3","mango")))
+
     val inst = new SparkStreamingExample
-    inst.process(conf,ssc)
+    inst.process(conf,ssc,statDF)
 
   }
 
